5320abac7bbb75b548410b4025d7bbcacbcdfbc2
zarei2025 <zande2025@gmail.com>
2021-12-06 12:28:21 +0330

    modal v1

---
+++ testapplication/Models/ItemTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testapplication.Models
+{
+    public class ItemTable
+    {
+        public int  Id{ get; set; }
+        public string Title { get; set; }
+        public string TypeTitle { get; set; }
+
+        public ItemTable(string typeTitle)
+        {
+            TypeTitle = typeTitle;
+        }
+
+        public ItemTable(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+}
